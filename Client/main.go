68fda0a168c2ede830fcc82cd6e2e10248ef5ea7
Rogelio Mtz <163707504+RogelioMtz@users.noreply.github.com>
2024-11-11 20:31:19 -0600

    Update main.go

--- Client/main.go
+++ Client/main.go
@@ -1,13 +1,9 @@
-// main.go
-// This is the main entry point for the GoTestServer application.
-// It sets up an HTTP server using the Gorilla Mux router and listens on localhost at port 8080.
-
 package main
 
 import (
 	"net/http"
 
-	api "github.com/RogelioMtz/server"
+	api "github.com/RogelioMtz/Go-WarehouseServer"
 )
 
 func main() {
